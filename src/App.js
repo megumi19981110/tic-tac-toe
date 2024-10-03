5959631dcd6829a777e1893e6e42dd3a1bedb7e7
miyazakimegumi <coffee1110milk1998@icloud.com>
2024-10-04 06:20:18 +0900

    クリックされたときに Square コンポーネントが呼び出す関数を記載

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 
 function Square({value}) {
-  return <button className="square">{value}</button>;
-  
+  return (
+    <button className="square" onClick={onSquareClick}>
+  {value}</button>);
+
   function handleClick() {
     setValue('X');
   }
