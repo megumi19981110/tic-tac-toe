3f6c28b7007b963d75cf1450d1b2f23a0a3bb8e1
miyazakimegumi <coffee1110milk1998@icloud.com>
2024-10-06 13:09:18 +0900

    Game 内の jumpTo 関数を更新して、currentMove を更新

--- src/App.js
+++ src/App.js
@@ -62,7 +62,8 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
