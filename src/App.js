619c310d2cd9c635baaaf0c7e04c76b5f7b12592
miyazakimegumi <coffee1110milk1998@icloud.com>
2024-10-06 12:56:12 +0900

    Board コンポーネント内の handleClick で setSquares と setXIsNext を呼び出しているところを、onPlay 関数への単一の呼び出しに置き換え

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
